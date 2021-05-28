38f5845981222e2cc40673295a2056ab61ebcac7
Murat Özakçil <murat@sanalyer.com>
2021-05-29 02:33:10 +0300

    matematik operatörleri

---
+++ matematik_operatorleri.py
@@ -0,0 +1,35 @@
+#BASİT TOPLAMA İŞLEMİ
+
+k = 34
+l = 54
+m = 58
+
+sonuc = k + l + m
+
+#TAM SAYI BÖLMESİ
+
+a = 345
+b = 36
+
+sonuc = a // b
+sonuc = a / b
+
+#kalanı bulma
+
+sonuc = 13 % 4
+
+#kalan sıfırsa çifttir
+
+sonuc = 4 % 2
+
+
+#üs bulma
+
+sonuc = 3 ** 8
+
+#karekök bulma
+
+sonuc = 64 ** 0.5
+
+
+print(sonuc)
